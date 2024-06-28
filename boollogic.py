3fd1a2e663d0be9467581ab173717db6a92f67c8
Tyler Cady <99786931+tyler-cady@users.noreply.github.com>
2024-06-28 09:41:15 -0400

    Update boollogic.py

--- boollogic.py
+++ boollogic.py
@@ -1,36 +1,15 @@
-# models.py
+from django import forms
+from .models import UnitTestMeasurement, MeasurementType
 
-from django.db import models
+class UnitTestMeasurementForm(forms.ModelForm):
+    class Meta:
+        model = UnitTestMeasurement
+        fields = ['measurement_type', 'bool_value', 'text_value']
 
-class MainModel(models.Model):
-    name = models.CharField(max_length=100)
-
-class Image(models.Model):
-    main_model = models.ForeignKey(MainModel, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images/')
-    numeric_value = models.CharField(
-        max_length=10,
-        validators=[RegexValidator(r'^\d+$', 'Only numeric characters are allowed.')]
-    )
-    description = models.CharField(max_length=255, null=True, blank=True)
-    uploaded_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-
-class Measurement(models.Model):
-    main_model = models.ForeignKey(MainModel, on_delete=models.CASCADE)
-    measurement_type = models.BooleanField(default=False)  # True for type A, False for type B
-    value = models.FloatField()
-
-    def save(self, *args, **kwargs):
-        if self.measurement_type:  # Type A
-            self.handle_type_a()
-        else:  # Type B
-            self.handle_type_b()
-        super().save(*args, **kwargs)
-
-    def handle_type_a(self):
-        # Add your logic for handling type A measurements
-        self.value = self.value * 2  # Example: just doubling the value
-
-    def handle_type_b(self):
-        # Add your logic for handling type B measurements
-        self.value = self.value + 10  # Example: adding 10 to the value
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.measurement_type:
+            if self.instance.measurement_type.unit_type == 'bool':
+                self.fields['text_value'].widget = forms.HiddenInput()
+            else:
+                self.fields['bool_value'].widget = forms.HiddenInput()
